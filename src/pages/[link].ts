f6ca10bbb07db5fae4c428880309fbdb01d46706
Ash Entwisle <ash.entwisle@protonmail.com>
2024-01-14 21:39:44 +0000

    updated pages
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/pages/[link].ts
+++ src/pages/[link].ts
@@ -7,7 +7,15 @@ export async function GET({ params, request }: APIContext) {
     const link = params.link;
     const redir = Config.links.find((l: LLLink) => l.link === link);
 
-    if (redir) return { status: 302,
-        headers: new Headers({ Location: redir.href, })
+    if (redir) {
+        let headers = new Headers();
+        
+        headers.set("og:title", Config.title);
+        headers.set("og:description", Config.about);
+        headers.set("og:url", request.url);
+        headers.set("Location", redir.href);
+
+        return { headers, status: 302 }
     }
+
 }
